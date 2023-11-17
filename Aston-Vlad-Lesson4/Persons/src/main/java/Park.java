dc4cd228b7f27d5135dfc8b8c2123eb56909e302
Vlad-Mezhevich <anarezpb@gmail.com>
2023-11-17 19:27:37 +0300

    Класс Сотрудники и Парк.
    Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
    Конструктор класса должен заполнять эти поля при создании объекта.
    Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
    
    Создать массив из 5 сотрудников.
    
    Пример:
    // вначале объявляем массив объектов
    Person[] persArray = new Person[5];
    // потом для каждой ячейки массива задаем объект
    persArray[0] = new Person("Ivanov Ivan", "Engineer",
                   "ivivan", "892312312", 30000, 30);
    persArray[1] = new Person(...);
    ...
    persArray[4] = new Person(...);
    
    С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
     Создать класс Park с внутренним классом, с помощью объектов которого можно хранить информацию об аттракционах, времени их работы и стоимости.

---
+++ Aston-Vlad-Lesson4/Persons/src/main/java/Park.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+public class Park {
+
+    Attractions attraction;
+    String timeWork;
+    Double price;
+
+    public Park(Attractions attraction, String timeWork, Double price) {
+        this.attraction = attraction;
+        this.timeWork = timeWork;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Парк атракционов = {" +
+                "Атракцион: " + attraction +
+                "; Время работы: " + timeWork +
+                "; Стоимость: " + price +
+                '}';
+    }
+
+
+}
