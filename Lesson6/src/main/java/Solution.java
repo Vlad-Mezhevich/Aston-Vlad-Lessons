5ef9e61cdc72bb823554760189de9e415e17c479
Vlad-Mezhevich <anarezpb@gmail.com>
2023-11-26 22:45:30 +0300

    Lesson6 File CSV
    Реализовать сохранение данных в csv файл; Реализовать загрузку данных из csv файла. Файл читается целиком.
    
    Структура csv файла:
    Строка заголовок с набором столбцов
    Набор строк с целочисленными значениями
    Разделитель между столбцами - символ точка с запятой (;)
    
    (Пример см. на скриншоте)
    
    Для хранения данных использовать класс вида:
    public class AppData {
       private String[] header;
       private int[][] data;
       // ...
    }
    Если выполняется save(AppData data), то старые данные в файле полностью перезаписываются.

---
+++ Lesson6/src/main/java/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String path1 = "file1.csv";
+        String path2 = "file2.csv";
+        String[] header1 = {"Value1", "Value2", "Value3"};
+        int[][] data1 = {
+                {11, 12, 13},
+                {21, 22, 23}};
+        AppData table1 = new AppData(header1, data1);
+        // Запишем информацию из объекта table1 в два файла
+        table1.save(path1);
+        table1.save(path2);
+        // Теперь прочитаем данные, записанные в файлы
+        AppData.readCSV(path1);
+        AppData.readCSV(path2);
+        /*
+        Данные одинаковы
+        Теперь запишем новые данные в файле file2.csv
+        */
+        String[] header2 = {"Value1", "Value2", "Value3", "Value4"};
+        int[][] data2 = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
+        AppData table2 = new AppData(header2, data2);
+        table2.save(path2);
+        AppData.readCSV(path2);
+    }
+
+}
