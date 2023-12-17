749fa88fe98d3429c339ba8f3c6334731ccdda62
Vlad-Mezhevich <anarezpb@gmail.com>
2023-12-17 19:11:19 +0300

    Lessot12_junit

---
+++ Lesson12junit/src/main/java/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число для вычисления факториала числа: ");
+        int number = scanner.nextInt();
+
+        long factorial = calculateFactorial(number);
+        System.out.println("Факториал числа " + number + " равен " + factorial);
+    }
+
+    static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else if (n > 0) {
+            return n * calculateFactorial(n - 1);
+        } else throw new IllegalArgumentException("Число не может быть отрицательным");
+    }
+}
